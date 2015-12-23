9a5eae5f681d2d573c41ae69196d5101f9e7ab77
Jason Sohl <jason@frozensoliddesigns.com>
2015-12-23 12:04:36 -0800

    improving performance with Adam's suggestions

--- public/js/flexbox-layout.js
+++ public/js/flexbox-layout.js
@@ -1,69 +1,43 @@
 var customGutters = document.getElementsByClassName('gutter-custom');
 
-for (var i = 0; i < customGutters.length; i++) {
-  var thisContainer = customGutters[i];
-  var gutterSize = thisContainer.getAttribute('data-gutter');
+var customGuttersLength = customGutters.length,
+    thisContainer,
+    gutterSize,
+    type = "col",
+    childElements;
 
-  var type = "col";
+for (var i = 0; i < customGuttersLength; i++) {
+  thisContainer = customGutters[i];
+  gutterSize = thisContainer.getAttribute('data-gutter');
+
   if ( (" " + thisContainer.className + " ").replace(/[\t\r\n\f]/g, " ").indexOf(" rows ") > -1 ) {
     type = "row";
   }
 
-  var childElements = thisContainer.querySelectorAll('.flex-container > section');
+  childElements = thisContainer.querySelectorAll('.flex-container > section');
   setMargins(childElements, type, gutterSize);
-
-  // if (type == "col") {
-  //   for (var j = 0; j < childElements.length; j++) {
-  //     var thisCol = childElements[j];
-  //     thisCol.setAttribute("style","margin-right: "+gutterSize+"px;");
-
-  //     var boxes = thisCol.querySelectorAll('.box');
-
-  //     if (boxes.length) {
-  //       for (var k = 0; k < boxes.length; k++) {
-  //         var thisBox = boxes[k];
-  //         thisBox.setAttribute("style","margin-bottom: "+gutterSize+"px;");
-  //       }
-  //     }
-  //   }
-  // }
-
-  // if (type == "row") {
-  //   for (var l = 0; l < childElements.length; l++) {
-  //     var thisRow = childElements[l];
-  //     thisRow.setAttribute("style","margin-bottom: "+gutterSize+"px;");
-
-  //     var boxes = thisRow.querySelectorAll('.box');
-
-  //     if (boxes.length) {
-  //       for (var m = 0; m < boxes.length; m++) {
-  //         var thisBox = boxes[m];
-  //         thisBox.setAttribute("style","margin-right: "+gutterSize+"px;");
-  //       }
-  //     }
-  //   }
-  // }
 
 }
 
 function setMargins(elems, type, gutterSize) {
+  var el, boxes, thisBox;
   for (var i = 0; i < elems.length; i++) {
-    var el = elems[i];
+    el = elems[i];
     if (type === "col") {
-      el.setAttribute("style","margin-right: "+gutterSize+"px;");
+      el.style.marginRight = gutterSize+"px";
     } else {
-      el.setAttribute("style","margin-bottom: "+gutterSize+"px;");
+      el.style.marginBottom = gutterSize+"px";
     }
 
-    var boxes = el.getElementsByClassName('box');
+    boxes = el.getElementsByClassName('box');
 
     if (boxes.length) {
       for (var k = 0; k < boxes.length; k++) {
-        var thisBox = boxes[k];
+        thisBox = boxes[k];
         if (type === "col") {
-          thisBox.setAttribute("style","margin-bottom: "+gutterSize+"px;");
+          thisBox.style.marginBottom = gutterSize+"px";
         } else {
-          thisBox.setAttribute("style","margin-right: "+gutterSize+"px;");
+          thisBox.style.marginRight = gutterSize+"px";
         }
       }
     }
